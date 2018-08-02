8559cf001e064a4374724bb13e6078ae3ef7b31e
Mahtab Alam <alammahtab08@gmail.com>
2018-08-02 21:59:06 +0530

    Added log

--- app.js
+++ app.js
@@ -5,11 +5,21 @@ var app=express();
 var mysql = require('mysql');
  
 var connection = mysql.createConnection({
-  host     : 'localhost',
+  host     : 'quotes.cod51i9yqypz.us-east-1.rds.amazonaws.com',
   database : 'quotesdb',
   user     : 'awesome_user',
-  password : 'awesome_password'
+  password : 'awesome_password',
+  port     :  3306
 });
+
+connection.connect(function(err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+    }
+  
+    console.log('Connected to database.');
+  });
 
 app.get('/quotes', function (req, res) {
     connection.query('select * from quotes', function (error, results, fields) {
