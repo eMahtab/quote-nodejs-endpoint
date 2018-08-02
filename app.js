7103d8013387b370597f374c2bcea5e5660548a6
Mahtab Alam <alammahtab08@gmail.com>
2018-08-02 16:59:42 +0530

    Aded quotes endpoint

---
+++ app.js
@@ -0,0 +1,28 @@
+var express=require('express');
+
+var app=express();
+
+var mysql = require('mysql');
+ 
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  database : 'quotesdb',
+  user     : 'awesome_user',
+  password : 'awesome_password'
+});
+
+app.use(express.static('public'));
+
+app.get('/quotes', function (req, res) {
+    connection.query('select * from quotes', function (error, results, fields) {
+    if (error) throw error;
+    res.end(JSON.stringify(results));
+  });
+ });
+
+var port = process.env.PORT || 9091;
+
+var server=app.listen(port,function(req,res){
+    console.log("Catch the action at http://localhost:"+port);
+});
+
